614f9281bfffa431cdeb77feff9de163b6923486
jsowers13 <jsowers13@gmail.com>
2022-04-13 04:00:21 +0000

    added js logic to create glow

--- src/js/component/light.js
+++ src/js/component/light.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Light = () => {
+	const [selectedColor, setSelectedColor] = useState("none");
+
 	return (
-		<div className="vw-100 vh-100 bg-secondary d-flex flex-column align-items-center">
+		<div className="vw-100 vh-100 d-flex flex-column align-items-center">
 			<div className="lightTop"></div>
 			<div className="lightBox d-flex flex-column align-items-center rounded-3">
-				<button className="btn bg-danger rounded-circle"></button>
-				<button className="btn bg-warning rounded-circle"></button>
-				<button className="btn bg-success rounded-circle"></button>
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light bg-danger rounded-circle" +
+						(selectedColor === "red" ? " glowRed" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light bg-warning rounded-circle" +
+						(selectedColor === "yellow" ? " glowYellow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light bg-success rounded-circle" +
+						(selectedColor === "green" ? " glowGreen" : "")
+					}></div>
 			</div>
 		</div>
 	);
